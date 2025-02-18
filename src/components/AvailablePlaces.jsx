2207f93000cc853ac8934cadf4093d11dc9e7c29
Mohamed Nasr <mnasr0868@gmail.com>
2025-02-18 23:25:40 +0200

    practice that we can transform the data fetched from api and set the data with the new one and solving the setfetching false because if the prev status it will be false early befor the callback of the navigator will executes so we put it in the right place in the clausure and in the catch

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import Places from "./Places.jsx";
 import Error from "./Error.jsx";
+import { sortPlacesByDistance } from "../loc.js";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false);
@@ -15,15 +16,22 @@ export default function AvailablePlaces({ onSelectPlace }) {
         if (!response.ok) {
           throw new Error("Faild to fetch places");
         }
-        setAvilablePlaces(responseData.places);
+        navigator.geolocation.getCurrentPosition((position) => {
+          const sortedPlaces = sortPlacesByDistance(
+            responseData.places,
+            position.coords.latitude,
+            position.coords.longitude
+          );
+          setAvilablePlaces(sortedPlaces);
+          setIsFetching(false);
+        });
       } catch (error) {
         console.log(error);
         setError({
           message: error.message || "faild to fetch data try again later",
         });
+        setIsFetching(false);
       }
-
-      setIsFetching(false);
     }
     fetchingPlaces();
   }, []);
