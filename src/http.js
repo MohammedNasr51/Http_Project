c11a92b3829c27d7f4a367560508c5caddf91a46
Mohamed Nasr <mnasr0868@gmail.com>
2025-02-19 00:30:13 +0200

    adding the updating request and handle passing the new places selected to the backend

--- src/http.js
+++ src/http.js
@@ -6,3 +6,18 @@ export async function fetchAvailablePlaces() {
     }
     return responseData.places;
 }
+
+export async function UpdateUserPlaces(places) { 
+    const response = await fetch("http://localhost:3000/user-places", {
+        method: "PUT",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ places }),
+    });
+    const responseData = await response.json();
+    if (!response.ok) {
+        throw new Error("Faild to update places");
+    }
+    return responseData.message;
+}
