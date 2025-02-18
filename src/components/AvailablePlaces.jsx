d4e03f0ab95938f347a6c2eebd9b19e8c1940ff2
Mohamed Nasr <mnasr0868@gmail.com>
2025-02-18 17:58:08 +0200

    solve the problem of showing the fallback text instead of loading text initialy

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,20 +1,25 @@
-import { useState, useEffect } from 'react';
-import Places from './Places.jsx';
+import { useState, useEffect } from "react";
+import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false);
   const [avilablePlaces, setAvilablePlaces] = useState([]);
   useEffect(() => {
     async function fetchingPlaces() {
-      const response = await fetch('http://localhost:3000/places');
+      setIsFetching(true);
+      const response = await fetch("http://localhost:3000/places");
       const responseData = await response.json();
       setAvilablePlaces(responseData.places);
+      setIsFetching(false);
     }
     fetchingPlaces();
-  },[]);
+  }, []);
   return (
     <Places
       title="Available Places"
       places={avilablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
