8d1244506611ec46e108420e05f2c0835433cdf9
Mohamed Nasr <mnasr0868@gmail.com>
2025-02-18 17:42:56 +0200

    feching data from api using promise way

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,20 @@
+import { useState, useEffect } from 'react';
 import Places from './Places.jsx';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [avilablePlaces, setAvilablePlaces] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then((response) => {
+        return response.json();
+      }).then((responseData) => {
+        setAvilablePlaces(responseData.places);
+      })
+  },[]);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={avilablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
